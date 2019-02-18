011fba6486163584ac058db080f680d0f0082bcb
Роман Козлов <roman.kozlov@inbox.ru>
2019-02-18 22:57:06 +0300

    Add ScrollView

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    
+    var imageURL: URL? {
+        didSet {
+            ImageView.image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if ImageView.image == nil {
+            fetchImage()
+        }
+        
+    }
 
     @IBOutlet weak var ImageView: UIImageView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                ImageView.image = UIImage(data: imageData)
+            }
+        }
+        
+    }
     
 }
