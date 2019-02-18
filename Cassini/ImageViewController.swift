53838ddb4165dee80696f7aa3f7a063d80aa30f4
Admin <roman.kozlov@inbox.ru>
2019-02-18 17:47:56 +0300

    Add ImageView

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Admin on 18.02.2019.
+//  Copyright © 2019 Roman Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+}
